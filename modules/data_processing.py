634db3cb3a066cbbf7f81b5a2d0be4b11bc3aaf0
Liberontissauri <jp.liberon@gmail.com>
2022-05-01 21:48:00 +0100

    added convertFENCharacterToPiece function

--- modules/data_processing.py
+++ modules/data_processing.py
@@ -2,3 +2,25 @@ class Piece:
     def __init__(self, name, team):
         self.name = name
         self.team = team
+
+def convertFENCharacterToPiece(character):
+    piece_translation_dictionary = {
+        "p": lambda: Piece("pawn", "black"),
+        "P": lambda: Piece("pawn", "white"),
+        "n": lambda: Piece("knight", "black"),
+        "N": lambda: Piece("knight", "white"),
+        "b": lambda: Piece("bishop", "black"),
+        "B": lambda: Piece("bishop", "white"),
+        "r": lambda: Piece("rook", "black"),
+        "R": lambda: Piece("rook", "white"),
+        "q": lambda: Piece("queen", "black"),
+        "Q": lambda: Piece("queen", "white"),
+        "k": lambda: Piece("king", "black"),
+        "K": lambda: Piece("king", "bwhite"),
+    }
+    translated_piece = piece_translation_dictionary.get(character)
+    if translated_piece is None:
+        raise ValueError("Tried passing a value not included in the FEN standard")
+    return piece_translation_dictionary[character]()
+
+
