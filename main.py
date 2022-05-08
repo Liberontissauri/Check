3b250031349cbb3885c82285a923bb7834dde7bd
Liberontissauri <jp.liberon@gmail.com>
2022-05-08 12:31:09 +0100

    added rate_limiting

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI, HTTPException
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
+from fastapi import FastAPI, HTTPException, Depends
 from modules import data_processing as DataProcessing
 from modules import image_manipulation as ImageManipulation
 from starlette.responses import StreamingResponse
 import io
+
+import aioredis
+from fastapi_limiter import FastAPILimiter
+from fastapi_limiter.depends import RateLimiter
+
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup():
+    redis = await aioredis.from_url(config["REDIS_URL"], encoding="utf-8", decode_responses=True)
+    await FastAPILimiter.init(redis)
 
-@app.get("/api/board")
+rate_limit = [150, 60]
+if config["RATE_LIMITED"] == "FALSE":
+    rate_limit = [1000000000, 1]
+
+@app.get("/api/board", dependencies=[Depends(RateLimiter(times=rate_limit[0], seconds=rate_limit[1]))])
 async def getBoard(FEN: str, size: int, dark: str, light: str):
     if size > 1300:
         raise HTTPException(status_code=400, detail="Image size should be equal or lower to 1300px")
